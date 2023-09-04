f66947a8ed54558e42dd62fb619327ec7afe6a14
Oli PC <olvatgamer@gmail.com>
2023-09-04 20:56:25 -0300

    Initialized the app, landing page started

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+<main>
+    <a href="./">
+        Inicia Sesión
+    </a>
+    <div>
+        <h1>MCards</h1>
+        <span>Desarrolla tu <strong>consciencia</strong> musical para ser mejor musico. <br> <strong>Oye</strong> y <strong>crea</strong> al siguiente nivel.</span>
+        <div class="buttons">
+            <button>Conoce más</button>
+            <button>Registrate</button>
+        </div>
+    </div>
+</main>
+<section>
+</section>
+<style >
+    @import '../styles/landing.css';
+</style>
